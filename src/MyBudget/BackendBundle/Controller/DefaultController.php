32c80974819284fb6bfa5b6f9bac4146e27e6ffa
Emiliano Viada <emiliano.viada@excedesoft.com>
2012-12-20 13:00:26 -0300

    Added Chart controller and moved chart proof of concept to it

--- src/MyBudget/BackendBundle/Controller/DefaultController.php
+++ src/MyBudget/BackendBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace MyBudget\BackendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Ob\HighchartsBundle\Highcharts\Highchart;
 
 
 class DefaultController extends Controller
@@ -38,21 +37,6 @@ class DefaultController extends Controller
                     $out += $entry->getValue();
             }
         }
-
-        //Chart proof of concept
-        $series = array(
-            array("name" => "Gastos", "data" => array(1,2,4,5,6,3,8,7,6,7,8,9)),
-            array("name" => "Ingresos", "data" => array(1.5,2,3,6,6,5,9,8,7,7,9,9))
-        );
-
-        $chart = new Highchart();
-        $chart->chart->renderTo('linechart');  // The #id of the div where to render the chart
-        $chart->chart->type('area');
-        $chart->title->text('Últimos 12 meses');
-        $chart->xAxis->title(array('text'  => "Meses"));
-        $chart->xAxis->categories(array('Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'));
-        $chart->yAxis->title(array('text'  => "$$$"));
-        $chart->series($series);
         
         return $this->render('BackendBundle:Default:index.html.twig', array(
         	'since' => $oldest->getDateEntry(),
@@ -61,8 +45,7 @@ class DefaultController extends Controller
             'balance_class' => $balance_class,
             'today' => $today,
             'ing' => $ing,
-            'out' => $out,
-            'chart' => $chart
+            'out' => $out
     	));
     }
 }
